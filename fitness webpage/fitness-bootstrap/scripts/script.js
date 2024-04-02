7d94e363f9945825967a3535f5e49b0d2a08a1de
Oduran19 <145799422+Oduran19@users.noreply.github.com>
2024-04-01 22:36:41 -0400

    Add files via upload

---
+++ fitness webpage/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Oscar Duran
+    File Name: script.js
+    Date: 11/22/2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
